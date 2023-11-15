e811d8e97a7a443bda432f96b589cd65de97fa4e
RajaRehman054 <abdurrehman283560@gmail.com>
2023-11-15 21:59:12 +0500

    changed payment

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -5,16 +5,23 @@ var stripe = require('stripe')(process.env.STRIPE_KEY);
 var Transaction = require('../models/transaction');
 
 exports.createPayment = asyncHandler(async (req, res, next) => {
-	const paymentIntent = await stripe.paymentIntents.create({
-		amount: req.body.amount,
-		currency: 'usd',
-		automatic_payment_methods: {
-			enabled: true,
-		},
+	const session = await stripe.checkout.sessions.create({
+		line_items: [
+			{
+				price_data: {
+					currency: 'usd',
+					product_data: {},
+					unit_amount: req.params.amount,
+				},
+				quantity: 1,
+			},
+		],
+		mode: 'payment',
+		success_url: `${process.env.CLIENT_URL}/checkoutsuccess`,
+		cancel_url: `${process.env.CLIENT_URL}/bids`,
 	});
-	res.json({
-		clientSecret: paymentIntent.client_secret,
-	});
+
+	res.status(200).json({ url: session.url });
 });
 
 exports.getTransactionHistory = asyncHandler(async (req, res, next) => {
